04eeb3f788f1c48ec88830d754d01f369abaee08
Eric Clemmons <eric@smarterspam.com>
2016-12-04 13:38:42 -0600

    Move stories under __tests__

--- example/components/PrettyCode/__tests__/index.stories.js
+++ example/components/PrettyCode/__tests__/index.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf, action } from "@kadira/storybook";
 
-import PrettyCode  from "./";
+import PrettyCode  from "../";
 
 storiesOf("PrettyCode", module)
   .add("defaults to JavaScript", () => (
